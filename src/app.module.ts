9142596eca94a2d68e88322d57eb76fd152c5fac
Orphen <jonnhy_09@hotmail.com>
2021-09-05 19:15:53 -0500

    data access layer DB connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from './configs/data.access';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/configs/data.access.ts
+++ src/configs/data.access.ts
@@ -4,13 +4,14 @@ import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm
 export default class TypeOrmConfig {
   static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
     return {
-      type: 'postgres',
+      type: 'mysql',
       host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
+      port: +configService.get('DB_PORT'),
       username: configService.get('DB_USER'),
       password: configService.get('DB_PASSWORD'),
       database: configService.get('DB_NAME'),
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      ssl: true,
       synchronize: true,
       logging: true,
     };
